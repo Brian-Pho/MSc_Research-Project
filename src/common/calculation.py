3b800c84cdc97cea6a4436aea71971e1405ce8bf
Brian Pho <brianpho7@gmail.com>
2021-06-03 16:59:04 +0000

    Update PLS and MI scripts

---
+++ src/common/calculation.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+def multiply(v1, v2):
+    return v1 * v2
+
+
+def normalize_vector(v):
+    return v / np.linalg.norm(v)
+
+
+def calc_cosine_similarity(v1, v2):
+    v1_u = normalize_vector(v1)
+    v2_u = normalize_vector(v2)
+    
+    return np.dot(v1_u, v2_u)
+
+
+def calc_norm_euclidean(v1, v2):
+    v1_u = normalize_vector(v1)
+    v2_u = normalize_vector(v2)
+    
+    return np.linalg.norm(v1_u - v2_u)
+
+
+def compare_age_similarity(all_ages, bin_1, bin_2, bin_3, similarity_func):
+    comparisons = []
+    comparisons.append(similarity_func(all_ages, bin_1))
+    comparisons.append(similarity_func(all_ages, bin_2))
+    comparisons.append(similarity_func(all_ages, bin_3))
+    comparisons.append(similarity_func(bin_1, bin_2))
+    comparisons.append(similarity_func(bin_1, bin_3))
+    comparisons.append(similarity_func(bin_2, bin_3))
+    
+    if similarity_func == multiply:
+        return comparisons
+    
+    matrix = np.zeros((4, 4))
+    matrix[np.triu_indices_from(matrix, k=1)] = comparisons
+    matrix = matrix + matrix.T
+    return matrix
--- src/common/plotting.py
+++ src/common/plotting.py
@@ -1,4 +1,6 @@
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from nilearn import plotting, datasets
 
 power = datasets.fetch_coords_power_2011()
@@ -9,8 +11,8 @@ def plot_connections(connections, vmin, vmax, mi_mode=False):
     fc = create_power_fc_matrix(connections)
     cmap = 'bone' if mi_mode else 'bwr'
 
-    plot_fc_matrix(fc, cmap)
-    plot_fc_graph(fc, vmin, vmax, cmap)
+    plot_fc_matrix(fc, vmin, vmax, cmap)
+    plot_fc_graph(fc, vmin, vmax)
 
     if not mi_mode:
         positive_edges = np.clip(fc, 0, np.max(fc))
@@ -28,23 +30,23 @@ def plot_connections(connections, vmin, vmax, mi_mode=False):
     plot_node_strengths(node_strength, 0.5, "Greens")
 
 
-def plot_fc_matrix(fc, cmap):
-    plotting.plot_matrix(fc, colorbar=True, cmap=cmap)
+def plot_fc_matrix(fc, vmin=None, vmax=None, cmap='bwr'):
+    plotting.plot_matrix(fc, vmin=vmin, vmax=vmax, colorbar=True, cmap=cmap)
 
 
-def plot_fc_graph(fc, emin, emax, cmap):
+def plot_fc_graph(fc, emin=None, emax=None, cmap='bwr'):
     plotting.plot_connectome(fc, coords, node_size=5, colorbar=True,
                              edge_vmin=emin, edge_vmax=emax, edge_cmap=cmap)
 
 
 def plot_node_strengths(node_strength, threshold=None, cmap="Greens"):
     plotting.plot_markers(node_strength, coords, node_threshold=threshold,
-                          node_cmap=cmap)
+                          node_vmin=0, node_vmax=1, node_cmap=cmap)
 
 
 def create_power_fc_matrix(connections):
     fc = np.zeros((264, 264))
-    fc[np.triu_indices(264, k=1)] = connections
+    fc[np.triu_indices_from(fc, k=1)] = connections
     fc = fc + fc.T
     return fc
 
@@ -54,3 +56,8 @@ def convert_fc_to_node_strength(fc):
     node_strength = np.sum(np.abs(fc), axis=0)
     node_strength /= np.max(node_strength)
     return node_strength
+
+
+def plot_age_comparisons(comparisons):
+    f, ax = plt.subplots(figsize=(10, 10))
+    sns.heatmap(comparisons, annot=True, fmt=".3f", linewidths=.5, ax=ax)
