b99f40bfe7194052a0b66e8ab98488c9230cd2f1
Brian Pho <brianpho7@gmail.com>
2021-07-17 03:11:40 +0000

    Switch back to old age binning function

--- src/common/binning.py
+++ src/common/binning.py
@@ -1,17 +1,27 @@
 import numpy as np
 
 
-def bin_by_age(X, y, ages):
-    # DEPRECATED, USED 'bin_by_feature' INSTEAD
-    X_bin_1, y_bin_1 = _select_data(X, y, np.where(ages <= 9))
-    X_bin_2, y_bin_2 = _select_data(X, y, np.where(
-        np.logical_and(ages <= 12, ages > 9)))
-    X_bin_3, y_bin_3 = _select_data(X, y, np.where(ages > 12))
+def bin_by_age(X, y, ages, other_feature):
+    bin_1 = np.where(ages <= 9)
+    bin_2 = np.where(np.logical_and(9 < ages, ages <= 12))
+    bin_3 = np.where(12 < ages)
+    bin_indices = [bin_1, bin_2, bin_3]
+    
+    bins = [(X[bin_index], y[bin_index]) for bin_index in bin_indices]
+    
+    all_stats = [ages, other_feature]
+    for stat in all_stats:
+        binned_stat = [stat[bin_index] for bin_index in bin_indices]
+        
+        for bin_num, feature_bin in enumerate(binned_stat):
+            print(f'Bin {bin_num} Range: {np.min(feature_bin):.2f} -> {np.max(feature_bin):.2f}')
+#             print(f'Bin {bin_num}: {np.unique(feature_bin, return_counts=True)}')
+        print('---')
 
-    return [[X_bin_1, y_bin_1], [X_bin_2, y_bin_2], [X_bin_3, y_bin_3]]
+    return bins
 
 
-def bin_by_feature(X, y, feature, other_feature, num_bins=3):
+def equal_bin_by_feature(X, y, feature, other_feature, num_bins=3):
     # Create the bins
     indices = np.arange(feature.shape[0])
     bin_indices = np.split(indices, num_bins)
@@ -21,7 +31,7 @@ def bin_by_feature(X, y, feature, other_feature, num_bins=3):
     X, y = _select_data(X, y, sort_indices)
     
     # Apply the bins to the sorted data
-    bins = [_select_data(X, y, bin_index) for bin_index in bin_indices]
+    bins = [(X[bin_index], y[bin_index]) for bin_index in bin_indices]
     
     # Print bin statistics (age, sex, y)
     all_stats = [other_feature]
@@ -32,11 +42,6 @@ def bin_by_feature(X, y, feature, other_feature, num_bins=3):
         for bin_num, feature_bin in enumerate(binned_stat):
             print(f'Bin {bin_num} Range: {np.min(feature_bin):.2f} -> {np.max(feature_bin):.2f}')
 #             print(f'Bin {bin_num}: {np.unique(feature_bin, return_counts=True)}')
-            
         print('---')
         
     return bins
-
-
-def _select_data(X, y, indices):
-    return X[indices], y[indices]
