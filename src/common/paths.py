0ffb057d06913db18a263e7a0c2c674fe27139b7
Brian Pho <bpho@owenlab-blue.uwo.ca>
2021-07-23 08:14:15 +0000

    Save model  weights and add circular graph visualization

--- src/common/paths.py
+++ src/common/paths.py
@@ -1,6 +1,7 @@
 import os.path
 
 BPHO_DIR = '/imaging3/owenlab/bpho'
+REPO_DIR = '/home/bpho/Documents/MSc_Research-Project'
 
 # Functional Connectivity
 POWER_FC = os.path.join(BPHO_DIR, 'python_power_fc')
@@ -8,3 +9,8 @@ YEO_FC = os.path.join(BPHO_DIR, 'python_yeo_fc')
 
 # WISC
 WISC = os.path.join(BPHO_DIR, 'Biobank Labels', 'Subjects_with_WISC.csv')
+BAD_SUBJECTS = os.path.join(BPHO_DIR, 'scratch_data', 'bad_subjects.csv')
+
+# Model Weights
+PLS_WEIGHTS = os.path.join(REPO_DIR, 'scratch_data', 'pls_weights.npy')
+RIDGE_WEIGHTS = os.path.join(REPO_DIR, 'scratch_data', 'ridge_weights.npy')
