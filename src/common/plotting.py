b82fd544c8bc858633f80b35baab9dffb315ac1d
Brian Pho <bpho@owenlab-yellow.uwo.ca>
2021-08-04 08:34:22 +0000

    Add Power atlas labels to glass brain

--- src/common/plotting.py
+++ src/common/plotting.py
@@ -1,20 +1,24 @@
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from nilearn import plotting, datasets
 
+from common.paths import POWER
+
 power = datasets.fetch_coords_power_2011()
 coords = np.vstack((power.rois['x'], power.rois['y'], power.rois['z'])).T
+power_labels = pd.read_csv(POWER, index_col='ROI')
+node_colors = power_labels['Color'].values.tolist()
 
 
-def plot_connections(connections, vmin, vmax, mi_mode=False):
+def plot_connections(connections, vmin, vmax, pos_neg=False):
     fc = create_power_fc_matrix(connections)
-    cmap = 'bone' if mi_mode else 'bwr'
 
-    plot_fc_matrix(fc, vmin, vmax, cmap)
+    plot_fc_matrix(fc, vmin, vmax)
     plot_fc_graph(fc, vmin, vmax)
 
-    if not mi_mode:
+    if pos_neg:
         positive_edges = np.clip(fc, 0, np.max(fc))
         positive_node_strength = convert_fc_to_node_strength(positive_edges)
         plot_fc_graph(positive_edges, 0, vmax, "YlOrRd")
@@ -35,8 +39,8 @@ def plot_fc_matrix(fc, vmin=None, vmax=None, cmap='bwr'):
 
 
 def plot_fc_graph(fc, emin=None, emax=None, cmap='bwr'):
-    plotting.plot_connectome(fc, coords, node_size=5, colorbar=True,
-                             edge_vmin=emin, edge_vmax=emax, edge_cmap=cmap)
+    plotting.plot_connectome(fc, coords, node_size=5, colorbar=True, node_color=node_colors,
+                             edge_vmin=emin, edge_vmax=emax, edge_cmap=cmap, edge_threshold='90%')
 
 
 def plot_node_strengths(node_strength, threshold=None, cmap="Greens"):
@@ -52,7 +56,6 @@ def create_power_fc_matrix(connections):
 
 
 def convert_fc_to_node_strength(fc):
-    # calculate normalized, absolute strength for each node
     node_strength = np.sum(np.abs(fc), axis=0)
     node_strength /= np.max(node_strength)
     return node_strength
