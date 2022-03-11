f0b16d95879eb4c603f6ab823743e0026e46b8fa
Brian Pho <bpho@owenlab-yellow.uwo.ca>
2022-03-11 10:01:55 +0000

    Clean up and update raw data images

--- src/common/plotting.py
+++ src/common/plotting.py
@@ -12,24 +12,24 @@ power_labels = pd.read_csv(POWER, index_col='ROI')
 node_colors = power_labels['Color'].values.tolist()
 
 
-def plot_connections(connections, vmin, vmax, pos_neg=False):
+def plot_connections(connections, vmin=None, vmax=None, pos_neg=False, threshold="99.7%"):
     """
     Plot functional connectivity connections in multiple ways (E.g. Matrix and glass-brain/graph).
     """
     fc = create_power_fc_matrix(connections)
 
-#     plot_fc_matrix(fc, vmin, vmax)
-    plot_fc_graph(fc, vmin, vmax)
+    plot_fc_matrix(fc, vmin, vmax)
+    plot_fc_graph(fc, vmin, vmax, threshold=threshold)
 
     if pos_neg:
         positive_edges = np.clip(fc, 0, np.max(fc))
         positive_node_strength = convert_fc_to_node_strength(positive_edges)
-        plot_fc_graph(positive_edges, 0, vmax, "YlOrRd")
+        plot_fc_graph(positive_edges, 0, vmax, "YlOrRd", threshold=threshold)
         plot_node_strengths(positive_node_strength, 0, "YlOrRd")
 
         negative_edges = np.clip(fc, np.min(fc), 0)
         negative_node_strength = convert_fc_to_node_strength(negative_edges)
-        plot_fc_graph(-negative_edges, 0, vmax, "PuBu")
+        plot_fc_graph(-negative_edges, 0, vmax, "PuBu", threshold=threshold)
         plot_node_strengths(negative_node_strength, 0, "PuBu")
 
 #     node_strength = convert_fc_to_node_strength(fc)
@@ -45,13 +45,13 @@ def plot_fc_matrix(fc, vmin=None, vmax=None, cmap='bwr'):
     plotting.plot_matrix(fc, vmin=vmin, vmax=vmax, colorbar=True, cmap=cmap)
 
 
-def plot_fc_graph(fc, emin=None, emax=None, cmap='bwr'):
+def plot_fc_graph(fc, emin=None, emax=None, cmap='bwr', threshold="99.7%"):
     """
     Plot functional connectivity graph where nodes in a brain are connected by edges 
     of varying strength.
     """
     plotting.plot_connectome(fc, coords, node_size=5, colorbar=True, node_color=node_colors,
-                             edge_vmin=emin, edge_vmax=emax, edge_cmap=cmap, edge_threshold='99.7%')
+                             edge_vmin=emin, edge_vmax=emax, edge_cmap=cmap, edge_threshold=threshold)
 
 
 def plot_node_strengths(node_strength, threshold=None, cmap="Greens"):
