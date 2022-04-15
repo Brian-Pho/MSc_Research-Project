98a1daa0dcafc20d08f04381d81626c686c15a92
Brian Pho <bpho@owenlab-yellow.uwo.ca>
2022-04-15 13:40:16 -0400

    Add modelel weights and saving kmeans into csv

--- src/common/binning.py
+++ src/common/binning.py
@@ -8,14 +8,14 @@ def get_age_bins_indices(ages):
     """
     Locate the subjects/indicies where the condition is true.
     """
-    bin_1 = np.where(ages <= 9)
-    bin_2 = np.where(np.logical_and(9 < ages, ages <= 12))
-    bin_3 = np.where(12 < ages)
-    bin_indices = [bin_1, bin_2, bin_3]
+#     bin_1 = np.where(ages <= 9)
+#     bin_2 = np.where(np.logical_and(9 < ages, ages <= 12))
+#     bin_3 = np.where(12 < ages)
+#     bin_indices = [bin_1, bin_2, bin_3]
     
-#     bin_1 = np.where(ages <= 10)
-#     bin_2 = np.where(10 < ages)
-#     bin_indices = [bin_1, bin_2]
+    bin_1 = np.where(ages <= 10)
+    bin_2 = np.where(10 < ages)
+    bin_indices = [bin_1, bin_2]
     
     return bin_indices
 
@@ -44,22 +44,23 @@ def bin_data(X, y, ages=None, include_all=False):
     Returns either the original data or the binned data.
     """
     if ages is not None and ages.any():
-        bins = bin_by_age(X, y, ages)
-        bin_1, bin_2, bin_3 = bins[0], bins[1], bins[2]
+#         bins = bin_by_age(X, y, ages)
+#         bin_1, bin_2, bin_3 = bins[0], bins[1], bins[2]
         
-        if include_all:
-            X_bins = [X, bin_1[0], bin_2[0], bin_3[0]]
-            y_bins = [y, bin_1[1], bin_2[1], bin_3[1]]
-            bin_labels = BIN_LABELS
-        else:
-            X_bins = [bin_1[0], bin_2[0], bin_3[0]]
-            y_bins = [bin_1[1], bin_2[1], bin_3[1]]
-            bin_labels = ONLY_BIN_LABELS
-
-#         bin_1, bin_2 = bins[0], bins[1]
-#         X_bins = [bin_1[0], bin_2[0]]
-#         y_all = [bin_1[1], bin_2[1]]
-#         bin_labels = ONLY_BIN_LABELS[:2]
+#         if include_all:
+#             X_bins = [X, bin_1[0], bin_2[0], bin_3[0]]
+#             y_bins = [y, bin_1[1], bin_2[1], bin_3[1]]
+#             bin_labels = BIN_LABELS
+#         else:
+#             X_bins = [bin_1[0], bin_2[0], bin_3[0]]
+#             y_bins = [bin_1[1], bin_2[1], bin_3[1]]
+#             bin_labels = ONLY_BIN_LABELS
+            
+        bins = bin_by_age(X, y, ages)
+        bin_1, bin_2 = bins[0], bins[1]
+        X_bins = [bin_1[0], bin_2[0]]
+        y_bins = [bin_1[1], bin_2[1]]
+        bin_labels = ONLY_BIN_LABELS[:2]
         
     else:
         X_bins = [X]
