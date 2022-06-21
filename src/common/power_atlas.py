3de22b96cbd636ab0d05839a2af921d0be8cf8fb
Brian Pho <bpho@owenlab-yellow.uwo.ca>
2022-06-21 06:14:36 -0400

    Add svg version of CGs, bin differences, and bin similarities

--- src/common/power_atlas.py
+++ src/common/power_atlas.py
@@ -160,3 +160,24 @@ def to_power_network_fc_matrix(fc_vector):
     fc_matrix[np.triu_indices_from(fc_matrix, k=0)] = fc_vector
     fc_matrix = fc_matrix + fc_matrix.T - np.diag(np.diag(fc_matrix))
     return fc_matrix
+
+
+def select_k_connections(fc_vector, k=10, mode='top'):
+    """
+    Selects the top/bottom K connections from a Power (network or not) connectivity vector.
+    
+    Fills the remaining connections with zero.
+    """
+    fc_vector_mode_k = None
+    mode_k_indicies = None
+    
+    if mode == 'top':
+        fc_vector_mode_k = np.zeros(fc_vector.shape)
+        mode_k_indicies = np.argsort(fc_vector)[-k:]
+    elif mode == 'bottom':
+        fc_vector_mode_k = np.full(fc_vector.shape, np.max(fc_vector))
+        mode_k_indicies = np.argsort(fc_vector)[:k]
+
+    fc_vector_mode_k[mode_k_indicies] = fc_vector[mode_k_indicies]
+    
+    return fc_vector_mode_k
